534555030a696b36e844868a594ea771303d44d7
poirazis <poirazis@gmail.com>
2023-02-02 17:27:33 +0200

    1.0.6-alpha

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,8 +1,6 @@
 <script>
-  import { getContext, createEventDispatcher, onMount } from 'svelte'
-    import { bubble } from 'svelte/internal';
-  import { fade } from "svelte/transition"
-
+  import { getContext , onMount } from 'svelte'
+  import CellGeneric from './lib/CellGeneric.svelte';
   import CellRelationship from './lib/CellRelationship.svelte';
 
   const { styleable, builderStore  } = getContext("sdk")
@@ -10,144 +8,52 @@
   const columnInfo = getContext("SuperTableColumnInfo")
   const tableStore = getContext("tableStore")
 
-  export let value, required = true
+  export let value, rowKey, required = true
   export let valueColor
   export let editable
   export let cellType
   export let viewType
 
-  let autobind = value ?? true 
-  $: fontSize = $tableStore?.stylingOptions.rowFontSize ?? 14
-
-  const dispatch = createEventDispatcher()
-  let editing = false, original
-  let holdColor
-
-  onMount(() => {
-    original = value
-  })
-
-  function edit() {
-    editing = true
-    holdColor = $tableStore.stylingOptions.rowFontColor
-    $tableStore.stylingOptions.rowFontColor = "var(--spectrum-global-color-gray-100)"
-  }
-
-  function submit() {
-		if (value != original) {
-			dispatch('submit', value)
-		}
-    editing = false
-    $tableStore.stylingOptions.rowFontColor = holdColor
-  }
-
-  function keydown(event) {
-    if (event.key == 'Escape') {
-      event.preventDefault()
-      value = original
-      editing = false
-    }
-  }
-	
-	function focus(element) {
-		element.focus()
-	}
-
-  $: if (columnInfo && autobind) pickyBinder()
+  let autobind = true
 
   function pickyBinder()  {
     // Auto bind the value to the Super Column CellValue binding if plaed in one
     if ( $builderStore?.inBuilder ) {
-      let binding = "{{ [" + columnInfo?.name + "].[cellValue]" + " }}"
+      let binding = "{{ [" + columnInfo?.parentID + "].[cellValue]" + " }}"
       builderStore?.actions.updateProp("value", binding)
       value = binding
       autobind = false
     }
   }
-    // Append Styles Super Table Styling variables
-    $: styles = {
+  // Append Styles Super Table Styling variables
+  $: styles = {
     ...$component?.styles,
     normal: {
-      ...$component?.styles.normal,
-      "--super-table-cell-editable-height": fontSize + 14 + "px"
+      ...$component?.styles.normal
     },
   };
+
+  onMount( () => { if (columnInfo && !value && autobind) pickyBinder() } )
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div 
-  class="super-table-cell" 
-  on:click={edit}
-  class:in-edit-mode={editing} 
-  use:styleable={styles}>
+<div class="super-table-cell" use:styleable={styles}>
   <!-- Check valid placement  -->
   {#if !columnInfo || !tableStore}
     <p> Super Table Cells need to be placed inside Super Table Column Component </p>
   {:else}
-    {#if editable && editing }
-      <form on:submit|preventDefault={submit} on:keydown={keydown}>
-        <input style:--super-table-row-font-size={fontSize + "px"} class="inlineTextInput" bind:value on:blur={submit} {required} use:focus/>  
-      </form>
-    {:else if editable}
-      {value}
-    {:else}
-      {#if cellType == "relationship"}
-        <CellRelationship {viewType} {value} open={false} />
-      {:else}
-          {value} 
-      {/if}
-    {/if}
+    {value}
   {/if}
 </div>
 
 <style>
-
   .super-table-cell {
-    display: inline-flex;
-    flex-direction: column;
-    justify-content: center;
-    transition: all 1500ms;
-    min-height: var(--super-table-cell-editable-height);
-
-  }
-  .super-table-cell.in-edit-mode {
-
+    flex: 1;
+    display: block;
+    max-width: 350px !important;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 
-  .inlineTextInput {
-    font-size: var(--super-table-row-font-size);
-    font-weight: inherit;
-    font-style: italic;
-    color: inherit;
-    border: none;
-    background: none;
-    outline: none;
-    min-width: fit-content !important;
-    width: 60px;
-    padding: 4px 4px 6px 0px;
-    animation: fade-in 330ms ease-in-out;
-  }
-
-  .inlineTextInput:focus {
-    border: none;
-    border-bottom: 1px solid rgb(2, 101, 220);
-    color: var(--spectrum-global-color-gray-900);
-    outline: none;
-  }
-
-  .spectrum-Icon {
-    width: 12px;
-    height: 12px;
-    fill: var(--spectrum-global-color-gray-600);
-    display: none;
-  }
-
-  @keyframes fade-in {
-    0% {
-      opacity: 0%;
-    }
-    100% {
-      transform: 100%;
-    }
-  }
 </style>
---
+++ src/lib/CellGeneric.svelte
@@ -0,0 +1,87 @@
+<script>
+    export let value
+		export let open 
+		export let viewType
+		export let fontSize = "M"
+
+		let _value
+    $: {
+        try {
+            _value = JSON.parse(value)
+        } catch {
+            _value = null
+        }
+    }
+
+		function handleClick () {
+			open = !open;
+		}
+</script>
+
+	{#if _value}
+
+		{#if viewType == "tree"}
+
+			<ul class="spectrum-TreeView spectrum-TreeView--sizeS" style="width: 100%">
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<li on:click={handleClick} class="spectrum-TreeView-item" class:is-open={open}>
+						<span class="spectrum-TreeView-itemLink">
+							<svg class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-TreeView-itemIndicator" focusable="false" aria-hidden="true">
+								<use xlink:href="#spectrum-css-icon-Chevron100" />
+							</svg>
+							<span class="spectrum-TreeView-itemLabel">{ Object.keys(_value).length + " Related Records"}</span>
+						</span>
+						<ul class="spectrum-TreeView spectrum-TreeView--sizeS">
+							{#each _value as _row }
+							<li class="spectrum-TreeView-item">
+								<span class="spectrum-TreeView-itemLink">
+									<span class="spectrum-TreeView-itemLabel"> {_row["primaryDisplay"]} </span>
+								</span>            
+							</li>
+							{/each}
+						</ul>
+					</li>
+			</ul>
+		
+		{:else if viewType == "list"}
+
+			<nav>
+				<ul class="spectrum-SideNav">
+					{#each _value as _row }
+					<li class="spectrum-SideNav-item">
+						<span class="spectrum-SideNav-itemLink"> {_row["primaryDisplay"]} </span>
+					</li>
+					{/each}
+				</ul>
+			</nav>
+		
+		{/if}
+
+	{:else}
+		<p> No Related Records found </p>
+	{/if}
+
+	<style>
+
+		.spectrum-TreeView {
+			margin-top: 0px;
+			margin-bottom: 0px;
+		}
+	.spectrum-TreeView-itemLink {
+			height: 18px; 
+			padding-top: 0px;
+			padding-bottom: 0px;
+			padding-left: 20px;
+		}
+
+	.spectrum-TreeView-itemLink::before {
+			height: 18px;
+			padding-top: 0px;
+			padding-bottom: 0px;
+		}
+
+		.spectrum-TreeView-itemLabel {
+			line-height: 12px;
+			margin: 0px;
+		}
+	</style>
