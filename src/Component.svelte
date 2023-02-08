48a67cedf7ee9a4b1633cedff6d2ea730bf5ba14
poirazis <poirazis@gmail.com>
2023-02-08 09:52:39 +0200

    1.0.13-alpha

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,59 +1,106 @@
 <script>
   import { getContext , onMount } from 'svelte'
-  import CellGeneric from './lib/CellGeneric.svelte';
-  import CellRelationship from './lib/CellRelationship.svelte';
+  import CellString from './lib/CellString.svelte';
 
-  const { styleable, builderStore  } = getContext("sdk")
   const component = getContext("component")
-  const columnInfo = getContext("SuperTableColumnInfo")
-  const tableStore = getContext("tableStore")
-
+  const { styleable, builderStore  } = getContext("sdk")
+  const columnContext = getContext("columnContext")
+  const tableDataChangesStore = getContext("tableDataChangesStore")
+  
   export let value, rowKey, required = true
   export let valueColor
   export let editable
   export let cellType
   export let viewType
 
-  let autobind = true
+  export let fontColor, fontSize, isBold, isUnderline, isItalic 
 
-  function pickyBinder()  {
-    // Auto bind the value to the Super Column CellValue binding if plaed in one
-    if ( $builderStore?.inBuilder ) {
-      let binding = "{{ [" + columnInfo?.parentID + "].[cellValue]" + " }}"
-      builderStore?.actions.updateProp("value", binding)
-      value = binding
-      autobind = false
-    }
-  }
+  let autobind = true
+  let originalValue
+
   // Append Styles Super Table Styling variables
   $: styles = {
     ...$component?.styles,
     normal: {
-      ...$component?.styles.normal
+      ...$component?.styles.normal,
+      ...generateStylingVariables()
     },
   };
 
-  onMount( () => { if (columnInfo && !value && autobind) pickyBinder() } )
+  // Component Function Definitions 
+  function pickyBinder()  {
+    // Auto bind the value to the Super Column CellValue binding if plaed in one
+    if ( $builderStore?.inBuilder ) {
+      let valueBinding = "{{ [" + columnContext?.columnID + "].[cellValue]" + " }}"
+      builderStore?.actions.updateProp("value", valueBinding)
+      let keyBinding = "{{ [" + columnContext?.columnID + "].[rowKey]" + " }}"
+      builderStore?.actions.updateProp("rowKey", keyBinding)
+      autobind = false
+    }
+  }
+
+  function generateStylingVariables () {
+    let styles = {}
+    if ( fontColor ) styles["--spectrum-table-m-regular-cell-text-color"] = fontColor
+    if ( isBold ) styles["--spectrum-table-m-regular-cell-text-weight"] = 700
+    if ( isItalic ) styles["--spectrum-table-m-regular-cell-text-style"] = "italic"
+    if ( isUnderline ) styles["--spectrum-table-m-regular-cell-text-decoration"] = "underline"
+    return styles
+  }
+
+  // Event Handlers 
+  function handleEnterEdit () {
+    console.log("Editing")
+  }
+
+  function handleSubmit ( event ) {
+    let newDataChange = {
+				rowKey: rowKey,
+				field: columnContext?.columnField,
+				originalValue: originalValue,
+				newValue: event.detail.newValue
+			}
+
+    // Get all other dataChanges, removing any previous changes of ours
+    let dataChanges = $tableDataChangesStore.filter( v => v.rowKey != rowKey || v.field != columnContext?.columnField)
+
+    $tableDataChangesStore = [...dataChanges, newDataChange]
+  }
+
+  function handleCancelEdit () {
+    console.log("Editing Cancelled")
+  }
+
+  onMount( () => { 
+      if (columnContext && !value && autobind) pickyBinder() 
+      originalValue = value
+    } )
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="super-table-cell" use:styleable={styles}>
   <!-- Check valid placement  -->
-  {#if !columnInfo || !tableStore}
-    <p> Super Table Cells need to be placed inside Super Table Column Component </p>
-  {:else}
-    {value}
+  {#if !columnContext}
+    <p> Super Table Cells need to be placed inside Super Table Column Components </p>
+  {:else} 
+    <CellString on:enterEdit={handleEnterEdit} on:submit={handleSubmit} on:cancelEdit={handleCancelEdit} {value} {editable} />
   {/if}
 </div>
 
 <style>
   .super-table-cell {
-    flex: 1;
-    display: block;
+    display: flex;
+    flex-direction: row;
+    justify-content: stretch;
     max-width: 350px !important;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    color: var(--spectrum-table-m-regular-cell-text-color);
+    font-weight: var(--spectrum-table-m-regular-cell-text-weight);
+    font-style: var(--spectrum-table-m-regular-cell-text-style);
+    text-decoration: var(--spectrum-table-m-regular-cell-text-decoration);
+    width: 100%;
   }
 
 </style>
---
+++ src/lib/CellString.svelte
@@ -0,0 +1,92 @@
+<script>
+  import { createEventDispatcher, onMount } from 'svelte'
+
+  export let value
+  export let editable
+
+  const dispatch = createEventDispatcher()
+  let editing = false, original
+
+  onMount(() => {
+    original = value
+  })
+
+  function edit() {
+    if (editable) {
+      editing = true
+      dispatch("enterEdit")
+    }
+  }
+
+  function submit(event) {
+    event.preventDefault()
+		if (value != original) {
+			dispatch('submit', { newValue: value })
+		}
+		
+    editing = false
+  }
+
+  function keydown(event) {
+    if (event.key == 'Escape') {
+      event.preventDefault()
+      value = original
+      editing = false
+      dispatch("cancelEdit")
+    } else if ( event.key == "Enter" ) {
+      event.preventDefault()
+      editing = false
+      dispatch('submit', { newValue: value })
+    }
+  }
+	
+	function focus(element) {
+		element.focus()
+	}
+</script>
+
+{#if editing}
+  <div class="inline-wrapper">
+    <input class="inline-edit" on:keydown={keydown} bind:value on:blur={submit} use:focus/>
+  </div>
+{:else}
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class="inline-value" on:click={edit}>
+    {value}
+  </div>
+{/if}
+
+<style>
+  .inline-wrapper {
+    width: 100%;
+  }
+
+  .inline-value { 
+    padding: 4px 0px;
+    width: 100%;
+  }
+
+  .inline-edit {
+    background-color: inherit;
+    font-size: inherit;
+    color: inherit;
+    font-weight: inherit;
+    text-align: inherit;
+    box-shadow: none;
+    box-sizing: border-box;
+    width: 100%;
+    border: var(--spectrum-textfield-border-size) solid;
+    border-color: var(--spectrum-alias-border-color);
+    border-radius: 4px;
+
+  }
+
+  .inline-edit:focus {
+    padding: 4px;
+    outline: none;
+    border-width: 1px;
+    border-style: solid;
+    border-color: var(--spectrum-alias-border-color-mouse-focus);
+    background-color: var(--spectrum-textfield-m-background-color, var(--spectrum-global-color-gray-50));
+  }
+</style>
