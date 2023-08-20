8d583d8157c8c4603d095a16b441b3a7b0f1399b
poirazis <poirazis@gmail.com>
2023-08-20 19:41:20 +0300

    Rel Column Picker

--- lib/SuperTableCell/cells/CellLink.svelte
+++ lib/SuperTableCell/cells/CellLink.svelte
@@ -15,28 +15,45 @@
 
   let anchor;
   let results
-  let allowNull = fieldSchema.constraints.presence ?? false;
   let relTableSchema 
   let relTableSeachColumn
   let filteredValue
+  let searchColumns 
+  let primaryDisplay 
+  let queryParams = {}
+  let limit = 10
 
   let editorState = fsm("Closed", {
     Open: { toggle: "Closed" },
     Closed: { toggle: "Open" },
   });
 
-  const loadTable = async () => {
-    results = await API.searchTable({
+  let timer;
+	const debounce = e => {
+    clearTimeout(timer);
+		timer = setTimeout(() => {
+      filteredValue = e.target.value;
+		}, 500);
+	} 
+
+  const loadTable = async ( tableId, filterValue, limit ) => {
+
+    queryParams = {}
+    queryParams[primaryDisplay] = filterValue ?? ""
+
+    results = API.searchTable({
       paginate: false,
-      tableId: fieldSchema.tableId,
-      limit: 50,
-      query: { fuzzy: { "Customer Name": filteredValue } }
+      tableId: tableId,
+      limit: Number (limit),
+      query: { fuzzy: queryParams }
     });
     return results;
   };
 
   const loadTableSchema = async () => {
     relTableSchema = await API.fetchTableDefinition(fieldSchema.tableId);
+    searchColumns = [ relTableSchema.primaryDisplay ]
+    primaryDisplay = relTableSchema.primaryDisplay
     return relTableSchema;
   }
 
@@ -63,31 +80,45 @@
 
   const getNextColumn = ( e , val ) => {
     e.stopPropagation();
-    relTableSeachColumn = "Surname"
+    searchColumns.push ( "Customer Email" );
+    searchColumns = searchColumns;
+  }
+
+  const setLimit = (e, val ) => {
+    e.stopPropagation();
+    limit = val
   }
 
   function openPicker(e) {
     e.stopPropagation();
     editorState.toggle();
     if ( $editorState == "Open" ) {
+      
       if (!relTableSchema) relTableSchema = loadTableSchema();
       if (!results || filteredValue ) results = loadTable();
     }
   }
 
-  $: results = loadTable(filteredValue);
+  function focus(element) {
+		element.focus()
+	}
 
-  $: console.log(relTableSeachColumn)
+  $: if ( inEdit && !primaryDisplay ) loadTableSchema();
+  $: if ( $editorState == "Open" ) loadTable( fieldSchema.tableId, filteredValue , limit)
+  $: if ( !inEdit && $editorState == "Open" ) { editorState.toggle(); filteredValue = ""; }
+
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div bind:this={anchor} class="control">
+
   {#if inEdit}
     <div class="inline-value">
       {#if value}
-        {#each value as row, idx}
+        {#each value as row }
           <div class="item">
             {row.primaryDisplay}
-            <Icon size="XS" name="Close" hoverable on:click={ (e) => unselectRow(e, row)}/>
+            <Icon size="XS" name="Unlink" hoverable color={"var(--spectrum-global-color-gray-50)"} on:click={ (e) => unselectRow(e, row)}/>
           </div>
         {/each}
       {/if}
@@ -96,46 +127,59 @@
 
     <Popover
       on:close={editorState.toggle}
-      {anchor}
-      useAnchorWidth
+      {anchor} 
+      align = "left"
       open={$editorState == "Open"}
     >
-      <div class="searchControl">
-        <div class="columnSelect">
-          <Icon hoverable name="ChevronLeft" size="S" on:click={(e) => getPreviousColumn(e)} />
-          {relTableSeachColumn}
-          <Icon hoverable name="ChevronRight" size="S" on:click={(e) => getNextColumn(e)} />
+      <div class="columnWrapper"> 
+      {#each searchColumns as searchColumn}
+      <div class="column">
+        <div class="searchControl">
+          <div class="columnSelect">
+            {searchColumn}
+          </div>
+          <input class="input" on:input={debounce} type="text" use:focus/>
+          <div class="pageSize">
+            <div class="pageSizeItem" class:selected={limit == 10} on:click={(e) => setLimit(e, 10)}> 10 </div>
+            <div class="pageSizeItem" class:selected={limit == 50} on:click={(e) => setLimit(e, 50)}> 50 </div>
+            <div class="pageSizeItem" class:selected={limit == 100} on:click={(e) => setLimit(e, 100)}> 100 </div>
+            <div class="pageSizeItem" class:selected={limit == 10000} on:click={(e) => setLimit(e, 10000)}> ALL </div>
+          </div>
         </div>
-        <input class="input" bind:value={filteredValue} type="text" />
-      </div>
-      <div class="options"> 
-        {#await results}
-          <CellSkeleton > Loading... </CellSkeleton>
-        {:then results}
-        {#key value}
-          {#if results.rows.length > 0 }
-            {#each results.rows as row, idx }
-              {#if !(rowSelected(row)) }
-                <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <div class="option" on:click={selectRow(row)}
-                >
-                { row[relTableSchema.primaryDisplay] } 
-                </div>
+        <div class="options"> 
+          {#await results}
+            <CellSkeleton > Loading... </CellSkeleton>
+            <CellSkeleton > Loading... </CellSkeleton>
+            <CellSkeleton > Loading... </CellSkeleton>
+          {:then results}
+            {#key value}
+              {#if results.rows.length > 0 }
+                {#each results.rows as row, idx }
+                  {#if !(rowSelected(row)) }
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <div class="option" on:click={selectRow(row)} >
+                      <Icon name="Link" size="S" color={"var(--primaryColor)"} />
+                      <div class="option text">
+                         {row[searchColumn]}
+                      </div>
+                    </div>
+                  {/if}
+                {/each}
               {/if}
-            {/each}
-          {/if}
-          {/key}
-        {:catch error}
-          <p style="color: red">{error.message}</p>
-        {/await}
+            {/key}
+          {:catch error}
+            <p style="color: red">{error.message}</p>
+          {/await}
+        </div>
       </div>
+      {/each}
+    </div>
     </Popover>
   {:else}
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="inline-value">
       {#if value}
         {#each value as row}
-          <div class="item">{row.primaryDisplay}</div>
+          <div class="item"> {row.primaryDisplay} </div>
         {/each}
       {/if}
     </div>
@@ -159,14 +203,26 @@
     padding-right: var(--super-table-cell-padding);
   }
 
+  .columnWrapper {
+    flex: auto;
+    display: flex;
+    align-items: stretch;
+    gap: 1rem;    
+  }
+  .column {
+    flex: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+  }
+
   .searchControl {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: stretch;
     min-height: 2rem;
-    width: 100%;
-    padding: 0.5rem;
+    padding: 0 0.5rem;
   }
 
   .columnSelect {
@@ -174,30 +230,76 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
-    min-height: 2rem;
     font-weight: 800;
+    min-height: 1.85rem;
   }
 
   .input {
-    min-height: 2rem;
+    min-height: 1.85rem;
+    min-width: none;
+    width: 100%;
+    box-sizing: border-box;
+    outline: none;
+    background: none;
+    border: 1px solid var(--primaryColor);
+    padding-left: 0.5rem;
+    color: inherit;
+    font: inherit;
+    cursor: pointer;
+    background-color: var(--spectrum-textfield-m-background-color, var(--spectrum-global-color-gray-50));
+  }
+
+  .pageSize {
+    flex: auto;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 0.5rem;
+  }
+  .pageSizeItem {
+    width: 24%;
+    color: var(--primaryColor);
+    display: flex;
+    justify-content: center;
+    border: 1px solid var(--primaryColor);
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .pageSizeItem.selected {
+    color: white;
+    background-color: var(--primaryColor);
   }
   .options {
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
     align-items: stretch;
-    padding: 0.5rem;
+    overflow-y: auto;
+    padding: 0.3rem;
+    gap: 0rem;
+  }
+  .option {
+    padding: 0.15rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    cursor: pointer;
   }
 
-  .option {
-    flex: auto;
-    display: flex;
-    justify-content: flex-start;
-    cursor: pointer;
-    padding: 0.25rem 0.5rem;
+ :global(.option > span) {
+    color: lime;
+  }
+  .option:hover,
+  .option.focused {
+    background-color: var(--spectrum-global-color-gray-200);
     border-radius: 4px;
   }
-  .option:hover {
-    background-color: var(--spectrum-global-color-gray-100);
+
+  .text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
   .item {
     flex: 0 0 auto;
@@ -205,13 +307,13 @@
     justify-content: center;
     align-items: center;
     border-radius: 4px;
-    background-color: #407c36;
+    background-color: darkcyan;
     color: whitesmoke;
     height: 60%;
     padding-left: 0.5rem;
     padding-right: 0.5rem;
     white-space: nowrap;
-    gap: 0.25rem;
+    gap: 0.5rem;
 
   }
 </style>
